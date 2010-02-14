34ee90afb0bf8fafabe7d1804aa696ef261b9fcb
drz4zg4 <drz4zg4@drz4zg4-laptop.(none)>
2010-02-14 20:35:59 +0100

    rozwiazane 24.

---
+++ 24.rb
@@ -0,0 +1,10 @@
+class Array
+  def permutations
+    return [self] if size < 2
+    perm = []
+    each { |e| (self - [e]).permutations.each { |p| perm << ([e] + p) } }
+    perm
+  end
+end
+
+p (0..9).to_a.permutations[999999].join
